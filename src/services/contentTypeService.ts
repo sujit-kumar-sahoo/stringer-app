377bc89c12306aa77218c12a1165f7a40663dde0
sujit <sujitsreekumar19@gmail.com>
2025-08-21 18:10:01 +0530

    new update

---
+++ src/services/contentTypeService.ts
@@ -0,0 +1,25 @@
+import api from '../utilis/api';
+interface ApiResponse {
+  success: boolean;
+  message?: string;
+  data?: any;
+}
+export const getContentTypes = async (): Promise<ApiResponse> => {
+  try {
+    const response = await api.get('/api/content_type/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.response?.data?.detail || error.response?.data?.message || 'Error fetching priorities.',
+    };
+  }
+};
